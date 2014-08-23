a4e84827ab74c438c697e6ba86d8cb318096703d
srhuevo <garoz.daniel@gmail.com>
2014-08-23 21:46:36 +0200

    AsyncTask para el video Acabado

--- MandoIOIO/src/com/i3c/mandoioio/TaskVideo.java
+++ MandoIOIO/src/com/i3c/mandoioio/TaskVideo.java
@@ -1,13 +1,49 @@
 package com.i3c.mandoioio;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.widget.ImageView;
 
-public class TaskVideo extends AsyncTask<Void, Void, Void>{
+public class TaskVideo extends AsyncTask<Void, Bitmap, Void>{
 
+	ImageView video;
+	DatagramSocket s;
+	byte[] buffer;
+	DatagramPacket reply;
+	
+	public TaskVideo(ImageView video, DatagramSocket s){
+		this.video = video;
+		this.s = s;
+        buffer = new byte[15000];
+        reply = new DatagramPacket(buffer, buffer.length);
+	}
+	
 	@Override
 	protected Void doInBackground(Void... params) {
-		//Prueba
+		try {
+			for(;;){
+				s.receive(reply);
+				InputStream in = new ByteArrayInputStream(buffer);
+				Bitmap bm = BitmapFactory.decodeStream(in);
+				publishProgress(bm);
+				if(isCancelled())
+					break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
-
+	
+     @Override
+     protected void onProgressUpdate(Bitmap... in) {
+    	 video.setImageBitmap(in[0]);
+     }
 }
